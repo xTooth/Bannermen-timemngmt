135fe04fd9249ca1af2bb9507c971e8247286d25
kim.jamsen <kim.jamsen@cgi.com>
2021-04-27 17:01:46 +0300

    User view navigation + hardcoded views for shifts
    Modified
    -AcceptedShifts now displays shifts some hardcoded values
    -AvailableShifts now displays some hardcoded values
    Added
    -more testdata

--- Scenes/AcceptedShifts.js
+++ Scenes/AcceptedShifts.js
@@ -1,15 +1,79 @@
 import React from 'react'
-import { TouchableOpacity, Text } from 'react-native';
+import { 
+   TouchableOpacity,
+   Text,
+   View,
+   SafeAreaView,
+   ScrollView,
+   StyleSheet, 
+} from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import { ListItem } from 'react-native-elements'
+
+
+
 
 const AcceptedShifts = () => {
+
    const goToAvailableShifts = () => {
       Actions.AvailableShifts()
    }
+
+   const data = require('../testdata/accepted-shifts.json')
+
+   const ListRender = () => {
+      return(
+         data.map((l, i) => (
+           <ListItem key={i} bottomDivider>
+             <ListItem.Content>
+               <ListItem.Title>{l.name}</ListItem.Title>
+               <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
+               <Text>Shift starts at:{l.startTime}</Text>
+               <Text>Shift ends at:{l.endTime}</Text>
+               <Text>Duration:{l.Duration}</Text>
+               <Text>Location:{l.Place}</Text>
+               <Text>Apply by this time:{l.LastTimeToApply}</Text>
+               <View style={styles.buttonStyleContainer}>
+               </View>
+             </ListItem.Content>
+           </ListItem>
+         ))
+         
+   )}
+
+
    return (
-      <TouchableOpacity style = {{ margin: 128 }} onPress = {goToAvailableShifts}>
-         <Text>This is AcceptedShifts!</Text>
-      </TouchableOpacity>
-   )
-}
+      <SafeAreaView style={styles.container}>
+      <View>
+         <TouchableOpacity style = {{margin: 30}} onPress = {goToAvailableShifts}>
+         <Text>AvailableShifts</Text>
+         </TouchableOpacity>   
+      </View>
+         <ScrollView style={styles.scrollView}>
+         <ListRender/>
+         </ScrollView>
+    </SafeAreaView>
+  )
+
+
+ 
+   
+   }
+
+   const styles = StyleSheet.create({
+      scrollView: {
+        backgroundColor: 'blue',
+        marginHorizontal: 20,
+      },
+      text: {
+        fontSize: 42,
+      },
+      buttonStyleContainer: {
+       flex: 1,
+       flexDirection: 'row',
+       marginHorizontal: 20,
+        marginTop: 5,
+      }
+    })
+
 export default AcceptedShifts
--- Scenes/AvailableShifts.js
+++ Scenes/AvailableShifts.js
@@ -1,20 +1,80 @@
 import React from 'react'
-import { TouchableOpacity, Text, View } from 'react-native';
+import { 
+   TouchableOpacity,
+   Text,
+   View,
+   StyleSheet,
+   SafeAreaView,
+   ScrollView,
+   Alert
+   } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import { ListItem, Button } from 'react-native-elements'
 
 const AvailableShifts = () => {
    const goToAcceptedShifts = () => {
       Actions.AcceptedShifts()
    }
-   return (
-      <View>
-      <TouchableOpacity style = {{margin: 30}} onPress = {goToAcceptedShifts}>
-         <Text>AcceptedShifts</Text>
-      </TouchableOpacity>
-      <Text>This is the page for listing available Shifts</Text>
-         
-      </View>
+
+
+const data = require('../testdata/shifts.json')
+
+
+const ListRender = () => {
+   return(
+      data.map((l, i) => (
+        <ListItem key={i} bottomDivider>
+          <ListItem.Content>
+            <ListItem.Title>{l.name}</ListItem.Title>
+            <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
+            <Text>Shift starts at:{l.startTime}</Text>
+            <Text>Shift ends at:{l.endTime}</Text>
+            <Text>Duration:{l.Duration}</Text>
+            <Text>Location:{l.Place}</Text>
+            <Text>Apply by this time:{l.LastTimeToApply}</Text>
+            <View style={styles.buttonStyleContainer}>
+            <Button
+            title="Apply for this shift"
+            onPress={() => Alert.alert('Applied')}
+            />
+            <Button
+            title="Decline this shift"
+            onPress={() => Alert.alert('Declined')}
+            />
+            </View>
+          </ListItem.Content>
+        </ListItem>
+      ))
       
-   )
+)}
+
+   return (
+      <SafeAreaView style={styles.container}>
+      <View>
+         <TouchableOpacity style = {{margin: 30}} onPress = {goToAcceptedShifts}>
+         <Text>AcceptedShifts</Text>
+         </TouchableOpacity>   
+      </View>
+         <ScrollView style={styles.scrollView}>
+         <ListRender/>
+         </ScrollView>
+    </SafeAreaView>
+  )
 }
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: 'blue',
+    marginHorizontal: 20,
+  },
+  text: {
+    fontSize: 42,
+  },
+  buttonStyleContainer: {
+   flex: 1,
+   flexDirection: 'row',
+   marginHorizontal: 20,
+    marginTop: 5,
+  }
+})
 export default AvailableShifts
