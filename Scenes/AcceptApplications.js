f48959173a46d1797471be61f1e47ceda3498b22
kim.jamsen <kim.jamsen@cgi.com>
2021-04-28 10:17:55 +0300

    Added acceptedApplications

--- Scenes/AcceptApplications.js
+++ Scenes/AcceptApplications.js
@@ -1,15 +1,68 @@
 import React from 'react'
-import { TouchableOpacity, Text } from 'react-native';
-import { Actions } from 'react-native-router-flux';
+import { 
+   Text,
+   View,
+   StyleSheet,
+   SafeAreaView,
+   ScrollView,
+   Alert
+ } from 'react-native';
+ import { ListItem, Button } from 'react-native-elements'
 
 const AcceptApplications = () => {
-   const goToCreateShifts = () => {
-      Actions.CreateShifts()
+
+   const data = require('../testdata/shift-requests.json')
+
+   const ListRender = () => {
+      return(
+         data.map((l, i) => (
+           <ListItem key={i} bottomDivider>
+             <ListItem.Content>
+               <ListItem.Title>{l.name}</ListItem.Title>
+               <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
+               <Text>Employee: {l.EmployeeName}</Text>
+               <Text>Location:{l.Location}</Text>
+               <Text>Shift:{l.ShiftAppliedFor}</Text>
+               <Text>Duration:{l.Duration}</Text>
+               <Text>Total amount of applicants:{l.TotalNumberOfApplicants}</Text>
+               <View style={styles.buttonStyleContainer}>
+               <Button
+               title="Accept application"
+               onPress={() => Alert.alert('Accepted')}
+               />
+               <Button
+               title="Decline application"
+               onPress={() => Alert.alert('Declined')}
+               />
+               </View>
+             </ListItem.Content>
+           </ListItem>
+         ))
+         
+   )}
+   
+      return (
+         <SafeAreaView style={styles.container}>
+            <ScrollView style={styles.scrollView}>
+            <ListRender/>
+            </ScrollView>
+       </SafeAreaView>
+     )
    }
-   return (
-      <TouchableOpacity style = {{ margin: 128 }} onPress = {goToCreateShifts}>
-         <Text>Return to Create Shifts</Text>
-      </TouchableOpacity>
-   )
-}
+   
+   const styles = StyleSheet.create({
+     scrollView: {
+       backgroundColor: 'blue',
+       marginHorizontal: 20,
+     },
+     text: {
+       fontSize: 42,
+     },
+     buttonStyleContainer: {
+      flex: 1,
+      flexDirection: 'row',
+      marginHorizontal: 20,
+       marginTop: 5,
+     }
+   })
 export default AcceptApplications
